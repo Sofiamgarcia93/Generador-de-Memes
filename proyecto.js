eb118c90b97d7252b378ede47965ee4c82454670
Sofia <sofiamgarcia93@hotmail.com>
2021-05-01 16:41:00 -0300

    agrego funcionalidades JS panel de Texto

--- proyecto.js
+++ proyecto.js
@@ -143,3 +143,143 @@ checkboxInferior.addEventListener('change',() =>{
       }
       
     })
+
+
+    //CAMBIAR DE FUENTE 
+
+    const selecDeFuente = document.querySelector('#selectTipografias');
+    
+
+    selecDeFuente.addEventListener('change', () =>{
+      topTitle.style.fontFamily = selecDeFuente.value;
+      bottomTitle.style.fontFamily = selecDeFuente.value;
+    })
+
+
+    //CAMBIAR TAMAÑO DE FUENTE NO ME FUNCIONA**********************
+    //***************************
+     //***************************
+      //***************************
+       //***************************
+
+
+    const tamanioFuente = document.getElementById('tamanioFuente');
+
+    tamanioFuente.addEventListener('input', () =>{
+      bottomTitle.style.fontSize = tamanioFuente.value;
+      bottomTitle.style.fontSize = tamanioFuente.value;
+    })
+
+    //ALINEACION FUENTE
+
+    const alineIzquierda = document.querySelector('.left');
+    const alineCenter    = document.querySelector('.center');
+    const alineDerecha  = document.querySelector('.right');
+
+    alineIzquierda.addEventListener('click', () =>{
+      topTitle.style = 'text-align: left;';
+      bottomTitle.style = 'text-align: left;';
+    })
+
+    alineCenter.addEventListener('click', () =>{
+      topTitle.style = 'text-align: center;';
+      bottomTitle.style = 'text-align: center;';
+    })
+
+    alineDerecha.addEventListener('click', () =>{
+      topTitle.style = 'text-align: right;';
+      bottomTitle.style = 'text-align: right;';
+    })
+
+    //COLOR DEL TEXTO
+
+
+    const colorDeTexto = document.getElementById('colorDeTexto');
+    const refSpanTexto = document.getElementById('refSpanTexto');
+
+    colorDeTexto.addEventListener('input', () =>{
+      topTitle.style.color = colorDeTexto.value;
+      bottomTitle.style.color = colorDeTexto.value;
+      refSpanTexto.textContent = colorDeTexto.value;
+    })
+
+
+    //COLOR DE FONDO
+
+    const colorDeFondo = document.getElementById('colorDeFondo');
+    const refSpanFondo = document.getElementById('refSpanFondo');
+
+
+    colorDeFondo.addEventListener('input', () =>{
+      topTitle.style.backgroundColor = colorDeFondo.value;
+      bottomTitle.style.backgroundColor = colorDeFondo.value;
+      refSpanFondo.textContent = colorDeFondo.value;
+
+    })
+
+
+
+    //FONDO TRANSPARENTE
+
+    const fondoTransparente = document.getElementById('fondoTransparente');
+
+    fondoTransparente.addEventListener('change', () =>{
+      if(fondoTransparente.checked){
+        topTitle.style.backgroundColor = 'transparent';
+        bottomTitle.style.backgroundColor = 'transparent';
+      }else{
+        topTitle.style.backgroundColor = colorDeFondo.value;
+        bottomTitle.style.backgroundColor = colorDeFondo.value;
+      }
+    })
+
+
+
+    //CONTORNO
+
+    const contornoNinguno = document.querySelector('.ninguno');
+    const contornoClaro = document.querySelector('.claro');
+    const contornoOscuro = document.querySelector('.oscuro');
+
+
+    contornoOscuro.addEventListener('click', () =>{
+      topTitle.style = '-webkit-text-stroke: 2px black;'
+      bottomTitle.style = '-webkit-text-stroke: 2px black;'
+    })
+
+    contornoClaro.addEventListener('click', () =>{
+      topTitle.style = '-webkit-text-stroke: 2px;'
+      bottomTitle.style = '-webkit-text-stroke: 2px;'
+    })
+
+    contornoNinguno.addEventListener('click', () =>{
+      topTitle.style = '-webkit-text-stroke:none;'
+      bottomTitle.style = '-webkit-text-stroke:none;'
+    })
+
+
+
+
+    //ESPACIADO NO ME SALEEEEEEEEEEEEEEEEE
+    //********************
+    //********************
+    //********************
+    //********************
+    //************************ */ */
+    //********************
+    const espaciado = document.getElementById('espaciado');
+
+    espaciado.addEventListener('input', () =>{
+      topTitle.style.paddingTop = espaciado.value;
+    })
+
+
+    //INTERLINEADO NI IDEA COMO HACERLO
+    //********************
+    //********************
+    //********************
+    //********************
+    //************************ */ */
+    //********************
+    const interlineado = document.getElementById('interlineado');
+
